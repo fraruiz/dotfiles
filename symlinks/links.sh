ca2151466af0910fee3d442d140a1f5fff9e516c
Francisco <franciscoruizlezcano@gmail.com>
2021-06-23 10:40:05 -0300

    building zim installation

--- symlinks/links.sh
+++ symlinks/links.sh
@@ -1,8 +1,7 @@
 #!/bin/user/env bash
 
 ln -s $DOTFILES_PATH/shell/.inputrc ~/.inputrc
-ln -s $DOTFILES_PATH/shell/bash/.bash_profile ~/.bash_profile
-ln -s $DOTFILES_PATH/shell/bash/.bashrc ~/.bashrc
+ln -s $DOTFILES_PATH/shell/zsh/.zimrc ~/.zimrc
 ln -s $DOTFILES_PATH/shell/zsh/.zlogin ~/.zlogin
 ln -s $DOTFILES_PATH/shell/zsh/.zshenv ~/.zshenv
 ln -s $DOTFILES_PATH/shell/zsh/.zshrc ~/.zshrc
@@ -10,3 +9,5 @@ ln -s $DOTFILES_PATH/git/.gitattributes ~/.gitattributes
 ln -s $DOTFILES_PATH/git/.gitconfig ~/.gitconfig
 ln -s $DOTFILES_PATH/git/.gitignore_global ~/.gitignore_global
 ln -s $DOTFILES_PATH/editors/.vimrc ~/.vimrc
+ln -s $DOTFILES_PATH/shell/bash/.bash_profile ~/.bash_profile
+ln -s $DOTFILES_PATH/shell/bash/.bashrc ~/.bashrc

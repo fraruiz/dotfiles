93011d742a4f1b06d4a739350ce376f87016010f
Francisco <franciscoruizlezcano@gmail.com>
2021-06-23 10:57:30 -0300

    fix typo

--- symlinks/links_macos.sh
+++ symlinks/links_macos.sh
@@ -1,9 +1,1 @@
 #!/bin/user/env bash
-
-ln -s $DOTFILES_PATH/opt/homebrew/bin/bash ~/bin/bash
-ln -s $DOTFILES_PATH/opt/homebrew/bin/gdate ~/bin/date
-ln -s $DOTFILES_PATH/opt/homebrew/bin/gfind ~/bin/find
-ln -s $DOTFILES_PATH/opt/homebrew/bin/gmake ~/bin/make
-ln -s $DOTFILES_PATH/opt/homebrew/bin/gsed ~/bin/sed
-ln -s $DOTFILES_PATH/opt/homebrew/bin/gtouch ~/bin/touch
-ln -s $DOTFILES_PATH/opt/homebrew/bin/zsh ~/bin/zsh
--- symlinks/links_macos_intel.sh
+++ symlinks/links_macos_intel.sh
@@ -1,8 +1,1 @@
 #!/bin/user/env bash
-
-ln -s $DOTFILES_PATH/usr/local/bin/bash ~/bin/bash
-ln -s $DOTFILES_PATH/usr/local/bin/gdate ~/bin/date
-ln -s $DOTFILES_PATH/usr/local/opt/findutils/bin/gfind ~/bin/find
-ln -s $DOTFILES_PATH/usr/local/opt/make/libexec/gnubin/make ~/bin/make
-ln -s $DOTFILES_PATH/usr/local/opt/gnu-sed/libexec/gnubin/sed ~/bin/sed
-ln -s $DOTFILES_PATH/usr/local/bin/gtouch ~/bin/touch

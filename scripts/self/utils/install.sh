a434755019234cd4cf598a82ebcb70fbbb582bbe
Francisco Ruiz <franciscoruizlezcano@gmail.com>
2021-07-12 23:37:02 -0300

    remove brew list

--- scripts/self/utils/install.sh
+++ scripts/self/utils/install.sh
@@ -2,17 +2,15 @@
 
 install_macos_custom() {
   output::answer "Installing needed gnu packages"
-  brew list bash || brew install bash | log::file "Installing brew bash"
-  brew list zsh || brew install zsh | log::file "Installing brew zsh"
-  brew list coreutils || brew install coreutils | log::file "Installing brew coreutils"
-  brew list make || brew install make | log::file "Installing brew make"
-  brew list gnu-sed || brew install gnu-sed | log::file "Installing brew gnu-sed"
-  brew list findutils || brew install findutils | log::file "Installing brew findutils"
-  brew list bat || brew install bat | log::file "Installing brew bat"
-  brew list hyperfine || brew install hyperfine | log::file "Installing brew hyperfine"
+  brew install coreutils | log::file "Installing brew coreutils"
+  brew install make | log::file "Installing brew make"
+  brew install gnu-sed | log::file "Installing brew gnu-sed"
+  brew install findutils | log::file "Installing brew findutils"
+  brew install bat | log::file "Installing brew bat"
+  brew install hyperfine | log::file "Installing brew hyperfine"
 
   output::answer "Installing mas"
-  brew list mas || brew install mas | log::file "Installing mas"
+  brew install mas | log::file "Installing mas"
 }
 
 install_linux_custom() {

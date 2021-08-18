02343015a1e015a976f1f77ea5c391af2e387602
Francisco Ruiz <40806815+franciscorzl@users.noreply.github.com>
2021-08-18 10:08:14 -0300

    Delete scripts/editors directory

--- scripts/editors/utils/files.sh
+++
@@ -1,9 +0,0 @@
-current_timestamp() { date +%s; }
-
-create_if_not_exists_file(){
-	[ ! -f "$1" ] && mkdir -p "$(dirname "$1")" && touch "$1"
-}
-
-ensure_if_exists_file(){
-  [ -f "$1" ] && mv "$1" "$1".$(current_timestamp).back
-}

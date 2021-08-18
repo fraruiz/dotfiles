f6996a9d76e2dff4916d990cfcef59533c8eb28a
Francisco Ruiz <40806815+franciscorzl@users.noreply.github.com>
2021-08-18 13:48:51 -0300

    add disable symlinks

---
+++ symlinks/unlinks.sh
@@ -0,0 +1,23 @@
+#!/bin/user/env bash
+
+source "$DOTFILES_PATH/symlinks/utils.sh"
+
+rm ~/.inputrc
+
+rm ~/.bashrc
+
+rm ~/.bash_profile
+
+rm ~/.zimrc
+
+rm ~/.zlogin
+
+rm ~/.zshenv
+
+rm ~/.zshrc
+
+rm ~/.gitattributes
+
+rm ~/.gitconfig
+
+rm ~/.gitignore_global

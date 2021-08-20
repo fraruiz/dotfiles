13b28987cdaf344b181d8552475d8f41bbc2d9c8
Francisco Ruiz <40806815+franciscorzl@users.noreply.github.com>
2021-08-20 14:20:19 -0300

    update output and add for

--- scripts/self/utils/install.sh
+++ scripts/self/utils/install.sh
@@ -1,29 +1,16 @@
 #!/bin/user/env bash
 
-brew_install() {
-  if brew list $1 &>/dev/null; then
-    brew install $1
-  else
-    output::answer "Installing $1"
-    brew install $1
-  fi
-
-  output::write "✅  $1 installed"
-}
-
 install_macos_custom() {
   output::answer "Installing needed gnu packages"
-  brew_install bash | log::file "Installing brew bash"
-  brew_install zsh | log::file "Installing brew zsh"
-  brew_install coreutils | log::file "Installing brew coreutils"
-  brew_install make | log::file "Installing brew make"
-  brew_install gnu-sed | log::file "Installing brew gnu-sed"
-  brew_install findutils | log::file "Installing brew findutils"
-  brew_install bat | log::file "Installing brew bat"
-  brew_install hyperfine | log::file "Installing brew hyperfine"
 
-  output::answer "Installing mas"
-  brew_install mas | log::file "Installing mas"
+  for package_manager in coreutils make gnu-sed findutils bat hyperfine mas; do
+    if ! brew list $1 &>/dev/null; then
+      output::answer "Installing $1"
+      log::file "Installing brew $1"
+      brew install $1
+    fi
+    output::write "✅ \`$1\` installed"
+  done
 }
 
 install_linux_custom() {

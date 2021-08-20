54fc5f55e2362870f8c3ba7903be0ee9f3ece027
Francisco Ruiz <40806815+franciscorzl@users.noreply.github.com>
2021-08-20 13:56:09 -0300

    add brew_install function

--- scripts/self/utils/install.sh
+++ scripts/self/utils/install.sh
@@ -1,32 +1,29 @@
 #!/bin/user/env bash
 
-install_macos_custom() {
-  if ! platform::command_exists brew; then
-    output::error "brew not installed, installing"
-
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  fi
-
-  if platform::is_macos_arm; then
-    export PATH="$PATH:/opt/homebrew/bin:/usr/local/bin"
+brew_install() {
+  if brew list $1 &>/dev/null; then
+    brew install $1
   else
-    export PATH="$PATH:/usr/local/bin"
+    output::answer "Installing $1"
+    brew install $1
   fi
 
-  mkdir -p "$HOME/bin"
+  output::write "✅  $1 installed"
+}
 
+install_macos_custom() {
   output::answer "Installing needed gnu packages"
-  brew list bash || brew install bash | log::file "Installing brew bash"
-  brew list zsh || brew install zsh | log::file "Installing brew zsh"
-  brew list coreutils || brew install coreutils | log::file "Installing brew coreutils"
-  brew list make || brew install make | log::file "Installing brew make"
-  brew list gnu-sed || brew install gnu-sed | log::file "Installing brew gnu-sed"
-  brew list findutils || brew install findutils | log::file "Installing brew findutils"
-  brew list bat || brew install bat | log::file "Installing brew bat"
-  brew list hyperfine || brew install hyperfine | log::file "Installing brew hyperfine"
+  brew_install bash | log::file "Installing brew bash"
+  brew_install zsh | log::file "Installing brew zsh"
+  brew_install coreutils | log::file "Installing brew coreutils"
+  brew_install make | log::file "Installing brew make"
+  brew_install gnu-sed | log::file "Installing brew gnu-sed"
+  brew_install findutils | log::file "Installing brew findutils"
+  brew_install bat | log::file "Installing brew bat"
+  brew_install hyperfine | log::file "Installing brew hyperfine"
 
   output::answer "Installing mas"
-  brew list mas || brew install mas | log::file "Installing mas"
+  brew_install mas | log::file "Installing mas"
 }
 
 install_linux_custom() {

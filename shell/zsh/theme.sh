988276e47bb25038e2d6f0fd640d3c158494803c
Francisco Ruiz <40806815+franciscorzl@users.noreply.github.com>
2021-09-02 17:03:01 -0300

    fix color branch

--- shell/zsh/theme.sh
+++ shell/zsh/theme.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env zsh
 
-THEME_MODE=${THEME_MODE:-"dark"}
+THEME_MODE=${THEME_MODE:-"light"}
+THEME_PROMPT_IN_NEW_LINE=${THEME_PROMPT_IN_NEW_LINE:-false}
 
 [[ $(echotc Co) -gt 100 ]] && support_color_tones=true || support_color_tones=false
 
@@ -9,23 +10,12 @@ status_icon_ko="Λ"
 git_no_changes_status="✓"
 git_dirty_status="✗"
 
-if [ "$THEME_MODE" = "dark" ]; then
-  git_branch_color="green"
-  git_dirty_status_color="yellow"
-  git_no_changes_status_color="white"
-  git_on_branch_color="white"
-  pwd_color="yellow"
-  status_icon_color="white"
-  [[ $support_color_tones = true ]] && ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=8' || ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=magenta'
-else
-  git_branch_color="black"
-  git_dirty_status_color="magenta"
-  git_no_changes_status_color="green"
-  git_on_branch_color="green"
-  pwd_color="black"
-  status_icon_color="black"
-  [[ $support_color_tones = true ]] && ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=8' || ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=magenta'
-fi
+git_branch_color="green"
+git_dirty_status_color="yellow"
+git_no_changes_status_color="white"
+git_on_branch_color="white"
+pwd_color="yellow"
+status_icon_color="white"
 
 local prompt_status="%(?:%F{green}$status_icon_ok:%F{red}$status_icon_ko)"
 
@@ -43,21 +33,20 @@ prompt_pwd() {
   print -n "%F{$pwd_color}${prompt_short_dir}"
 }
 
-
 prompt_git() {
   inside_git_repo="$(git rev-parse --is-inside-work-tree 2>/dev/null)"
 
   if [ "$inside_git_repo" ]; then
     branch_name=$(git symbolic-ref HEAD 2> /dev/null | awk 'BEGIN{FS="/"} {print $NF}')
+    git_branch="%F{$git_branch_color}$branch_name"
 
     git_status="%F{$git_no_changes_status_color}$git_no_changes_status"
     changed=$(git status --porcelain)
-
     if [ -n "${changed}" ]; then
         git_status="%F{$git_dirty_status_color}$git_dirty_status"
     fi
 
-    print -n "%F{$git_on_branch_color} on $branch_name $git_status"
+    print -n "%f on $git_branch $git_status"
   fi
 }
 

7a422053ba7092c768c583c1c32e26f2e3392cc2
Francisco Ruiz <francisco.ruiz@mercadolibre.com>
2022-01-26 10:47:49 -0300

    fix: remove restorer

--- scripts/core/dotfiles.sh
+++ scripts/core/dotfiles.sh
@@ -1,4 +1,4 @@
 dotfiles::list_bash_files() {
-  grep "#!/usr/bin/env bash" "$DOTFILES_PATH"/{bin,scripts,shell,installer,restorer} -R | awk -F':' '{print $1}'
+  grep "#!/usr/bin/env bash" "$DOTFILES_PATH"/{bin,scripts,shell,installer} -R | awk -F':' '{print $1}'
   find "$DOTFILES_PATH"/{bin,scripts,shell} -type f -name "*.sh"
 }

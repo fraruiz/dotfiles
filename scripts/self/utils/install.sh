e1cf011fc89472e511cd721938b0cdb239f34a39
Francisco Ruiz <francisco.ruiz@mercadolibre.com>
2023-02-27 09:28:48 -0300

    feat: setup git identity

--- scripts/self/utils/install.sh
+++ scripts/self/utils/install.sh
@@ -15,3 +15,15 @@ install_macos_custom() {
 install_linux_custom() {
   echo
 }
+
+setup_git_identity() {
+  output::answer "Setup your git identity"
+  echo -n "Enter your email: "
+  read email
+
+  echo -n "Enter your user: "
+  read user
+
+  git config --global user.name "$user"
+  git config --global user.email "$email"
+}

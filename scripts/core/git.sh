6674d879c853a84e3f43c504b40f28d0d9be5d89
Francisco Ruiz <francisco.ruiz@mercadolibre.com>
2023-05-03 12:34:33 -0300

    fix: updated self update and install

--- scripts/core/git.sh
+++ scripts/core/git.sh
@@ -5,3 +5,15 @@ git::is_in_repo() {
 git::current_branch() {
   git branch
 }
+
+git::setup_identity() {
+  output::answer "Setup your git identity"
+  echo -n "Enter your email: "
+  read email
+
+  echo -n "Enter your user: "
+  read user
+
+  git config --global user.name "$user"
+  git config --global user.email "$email"
+}
